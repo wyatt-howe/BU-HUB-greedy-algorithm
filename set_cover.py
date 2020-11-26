818c5ce01afb33dac8ac767fdb434aa70da0ef56
Wyatt Howe <43802211+wyatt-howe@users.noreply.github.com>
2020-11-26 16:02:04 -0500

    Update set_cover.py

--- set_cover.py
+++ set_cover.py
@@ -1,69 +1,114 @@
 import json
 import random
 
-CATALOG_PATH = 'catalog/set_everything.json'
+CATALOG_PATH = 'catalog/catalog_sorted.json'
 
-E = [0,0,0,0,0]
+catalog = json.load(open(CATALOG_PATH))
+
+#
+# Retrieve a course's full description from it's number, e.g. "CAS CH 182".
+#
+
+def lookup(num: str):
+    return next(filter(lambda e : e['number'] == num, catalog))
+
+#
+# Implementation of the greedy algorithm for set covering.
+#
+
+def set_cover_greedy(U):
+    courses = U  # ordered set of unit sets each representing a course
+
+    # Preprocess for the set sizes and unit lookups.
+    units = {}  # All units mapping to a list of courses that have each unit.
+    sizes = {}
+    for course_num, course in enumerate(courses):
+        l = len(course)
+        if not l in sizes:
+            sizes[l] = set()
+        sizes[l].add(course_num)
+
+        for unit in course:
+            if not unit in units:
+                units[unit] = []
+            units[unit].append(course_num)
+
+    # Find a satisfiable set of courses in linear time.
+    solution = []
+    for size in reversed(sorted(sizes.keys())[1:]):
+        S = sizes[size]
+        while len(S):
+            candidate = S.pop()
+            solution.append(candidate)
+            for unit in courses[candidate]:
+                for course_num in units[unit]:
+                    if course_num != candidate:
+                        course = courses[course_num]
+                        l = len(course)
+                        course.remove(unit)
+                        sizes[l].remove(course_num)
+                        sizes[l-1].add(course_num)
+                del units[unit]
+    return solution
+
+#
+# Remove the full course description and order as a set of sets.
+# Courses are represented as a set of their HUB units.
+#
+def set_problem_reduction(G):
+    return [set(v['units']) for v in G]
+
+#
+# Remove any BU HUB units that were already completed.
+#
+
+def filter_units(G, units):
+    return filter_units([
+        {'number': e['number'], 'units': list(filter( \
+            lambda s : not s == units[0] \
+        , e['units']))} for e in G
+    ], units[1:]) if len(units) else G
+
+def filter_school(G, units):
+    return filter_units([
+        {'number': e['number'], 'units': list(filter( \
+            lambda s : not s == units[0] \
+        , e['units']))} for e in G
+    ], units[1:]) if len(units) else G
+
+
+
+"""
+vvv  Modify the code below to get a recommendation  vvv
+"""
+
+# Load the catalog and remove unneeded units.  Example: I have already completed my philosophy unit.
+catalog = json.load(open(CATALOG_PATH))
+catalog = filter_units(catalog, ['Philosophical Inquiry and Life\'s Meanings'])
+catalog = filter_school(catalog, ['CAS'])
+
+# init mutablesx
+courses = list(range(len(catalog)))
 dump = ''
-while len(E) > 4 or dump.count("Writing-Intensive Course") < 2 or dump.count("Research and Information Literacy") < 2:
-    G = json.load(open(CATALOG_PATH))
-    G = [{'n': e['n'], 'u': list(filter( \
-            lambda s : not s == 'Philosophical Inquiry and Life\'s Meanings' \
-        , e['u']))} for e in G]
 
-    random.shuffle(G)
+# Search for courses.  Example: No more than 8 courses; I require multiple of the same unit for writing and reasearch literacy.
+while len(courses) > 8 or dump.count("Writing-Intensive Course") < 2 or dump.count("Research and Information Literacy") < 2:
+    random.shuffle(catalog)  # Delete this to prioritize low level courses (100s first and up)
 
-    # print(G[0])
+    # Invoke the set cover oracle
+    courses_set = set_problem_reduction(catalog)
+    courses = set_cover_greedy(courses_set)
 
-    F = [set(v['u']) for v in G]
-
-    print(F[0])
-
-    # exit()
-    from collections import defaultdict
-
-    # First prepare a list of all sets where each element appears
-    D = defaultdict(list)
-    for y,S in enumerate(F):
-        for a in S:
-            D[a].append(y)
-
-    L=defaultdict(set)
-    # Now place sets into an array that tells us which sets have each size
-    for x,S in enumerate(F):
-        L[len(S)].add(x)
-
-    E=[] # Keep track of selected sets
-    # Now loop over each set size
-    for sz in range(max(len(S) for S in F),0,-1):
-        if sz in L:
-            P = L[sz] # set of all sets with size = sz
-            while len(P):
-                x = P.pop()
-                E.append(x)
-                for a in F[x]:
-                    for y in D[a]:
-                        if y!=x:
-                            S2 = F[y]
-                            L[len(S2)].remove(y)
-                            S2.remove(a)
-                            L[len(S2)].add(y)
-
-    print(len(E))
-
-    catalog = json.load(open(CATALOG_PATH))
-
-    def lookup(num):
-        # print(num)
-        return next(filter(lambda e : e['n'] == num, catalog))
+    print(len(courses))
 
     dump = json.dumps([
-        G[e] for e in E
+        catalog[e] for e in courses
     ], indent=2, sort_keys=True)
 
-    print(len(E), dump.count("Writing-Intensive Course"), dump.count("Research and Information Literacy"))
+    print(len(courses), dump.count("Writing-Intensive Course"), dump.count("Research and Information Literacy"))
 
+# Write output to console in JSON format.
 dump = json.dumps([
-lookup(G[e]['n']) for e in E
-], indent=2, sort_keys=True)
+        lookup(catalog[e]['number']) for e in courses
+    ], indent=2, sort_keys=True)
 print(dump)
